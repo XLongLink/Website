0932730df8b41706b4a774e8cb090067e1ccebed
Sau1707 <83017316+Sau1707@users.noreply.github.com>
2022-05-20 00:13:03 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,12 +12,8 @@ const style: Style = {
 }
 
 import Connect from "../components/authenticate/connect";
-<<<<<<< Updated upstream
-import { useGetDashboardQuery } from "../components/redux/nextApi";
-import { setIOS } from "../components/redux/walletSlice";
 import Footbar from '../components/footbar/footbar'
-=======
->>>>>>> Stashed changes
+
 
 const Home: NextPage = () => {
     const [activeSection, setActiveSection] = useState(false)
@@ -37,59 +33,59 @@ const Home: NextPage = () => {
             {activeSection ?
                 <>
                     <Features
-                        side = "left"
-                        title = "Nickname"
-                        line_1 = "Chose an username"
-                        line_2 = "to use in all dApp"
-                        line_3 = "created with LongLink"
+                        side="left"
+                        title="Nickname"
+                        line_1="Chose an username"
+                        line_2="to use in all dApp"
+                        line_3="created with LongLink"
                     />
                     <Features
-                        side = "right"
-                        title = "Avatar"
-                        line_1 = "Coose any NFT and"
-                        line_2 = "set it as your Avatar"
+                        side="right"
+                        title="Avatar"
+                        line_1="Coose any NFT and"
+                        line_2="set it as your Avatar"
                     />
                     <Features
-                        side = "left"
-                        title = "Full data controll"
-                        line_1 = "Manage all the data"
-                        line_2 = "of different dApp"
-                        line_3 = "from a single console"
-                        line_4 = "+ Statistics"
+                        side="left"
+                        title="Full data controll"
+                        line_1="Manage all the data"
+                        line_2="of different dApp"
+                        line_3="from a single console"
+                        line_4="+ Statistics"
                     />
                 </> : <>
-                    <Features 
-                        side = "left"
-                        title = "User Authentication"
-                        line_1 = "Authenticate an user"
-                        line_2 = "with a single line"
-                        line_3 = "of code"
+                    <Features
+                        side="left"
+                        title="User Authentication"
+                        line_1="Authenticate an user"
+                        line_2="with a single line"
+                        line_3="of code"
                     />
                     <Features
-                        side = "right"
-                        title = "variable Storage"
-                        line_1 = "We offer a small"
-                        line_2 = "database to store"
-                        line_3 = "users variables"
-                        line_4 = ""
+                        side="right"
+                        title="variable Storage"
+                        line_1="We offer a small"
+                        line_2="database to store"
+                        line_3="users variables"
+                        line_4=""
                     />
-                    <Features 
-                        side = "left"
-                        title = "Frontend - Backend"
-                        line_1 = "Easy sdk offer the ability"
-                        line_2 = "to controll users in the"
-                        line_3 = "frontend and backend"
+                    <Features
+                        side="left"
+                        title="Frontend - Backend"
+                        line_1="Easy sdk offer the ability"
+                        line_2="to controll users in the"
+                        line_3="frontend and backend"
                     />
-                    <Features 
-                        side = "right"
-                        line_1 = "Access to user data with"
-                        line_2 = "a single line of code, get"
-                        line_3 = "NFT, token, avatar, name,"
-                        line_4 = ". . ."
+                    <Features
+                        side="right"
+                        line_1="Access to user data with"
+                        line_2="a single line of code, get"
+                        line_3="NFT, token, avatar, name,"
+                        line_4=". . ."
                     />
                 </>
             }
-            <Footbar/>
+            <Footbar />
         </>
     )
 }
