d9a85e27a9d9dfae4cc424fae16ba844bae5791f
Adriano Saurwein <89137470+Nano042101@users.noreply.github.com>
2022-06-10 08:50:24 +0200

    drop down menu on the navbar

--- components/navabar/navbar.tsx
+++ components/navabar/navbar.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
-import { AppBar, Box, Toolbar, Typography, Container, Button } from '@mui/material';
+import { AppBar, Box, Toolbar, Typography, Container, Button, Menu, IconButton, MenuItem } from '@mui/material';
 import Connect from "../authenticate/connect";
+import MenuIcon from '@mui/icons-material/Menu';
+import styles from "./navbar.module.css"
 
 /*  Bar used for navigating beetween different pages
     - Display the logo of the website
@@ -10,6 +12,17 @@ import Connect from "../authenticate/connect";
 */
 
 function NavBar() {
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+
     return (
         <>
             <AppBar position="relative" style={{ background: '#000' }}>
@@ -48,17 +61,75 @@ function NavBar() {
                                 Docs
                             </Button>
                             <Button
-                                key='Contact'
+                                key='About'
                                 href="/about"
                                 sx={{ my: 2, color: 'white', display: 'flex' }}
                             >
                                 About
                             </Button>
                         </Box>
+                        {/* DROPDOWN MENU */}
+                        <Box sx={{ flexGrow: 1, right: 0, position:'absolute', display: { xs: 'flex', sm:'flex', md: 'none' }}}>
+                            <IconButton
+                                size="large"
+                                aria-controls="menu-appbar"
+                                onClick={handleOpenNavMenu}
+                                color="inherit"
+                            >
+                              <MenuIcon />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorElNav}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={Boolean(anchorElNav)}
+                                onClose={handleCloseNavMenu}
+                                className={styles.menu}
+                            >  
+                                <MenuItem 
+                                    key='Home'
+                                    href="/"
+                                    onClick={handleCloseNavMenu}
+                                >
+                                    <Typography textAlign="center">Home</Typography>
+                                </MenuItem>
+                                <MenuItem 
+                                    key='News'
+                                    href="/news"
+                                    onClick={handleCloseNavMenu}
+                                >
+                                    <Typography textAlign="center">News</Typography>
+                                </MenuItem>
+                                <MenuItem 
+                                    key='Docs'
+                                    href="https://xlonglink.github.io/docs/api/props"
+                                    onClick={handleCloseNavMenu}
+                                >
+                                    <Typography textAlign="center">Docs</Typography>
+                                </MenuItem>
+                                <MenuItem 
+                                    key='About'
+                                    href="/about"
+                                    onClick={handleCloseNavMenu}
+                                >
+                                    <Typography textAlign="center">About</Typography>
+                                </MenuItem>
+                            </Menu>
+                        </Box>
+
+
                         {/* WALLETCONNECT BUTTON */}
-                        <div style={{ right: 0, position: "absolute" }}>
-                            <Connect />
-                        </div>
+                        <Box sx={{ right: 0, position:'absolute', display: { xs: 'none', sm: 'none', md: 'flex', lg: 'flex', xl: 'flex' }}}>
+                            <Connect/>
+                        </Box>
 
                     </Toolbar>
                 </Container>
