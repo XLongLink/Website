52a90d89482d58bf923eb8a336288aca7b98734c
Sau1707 <83017316+Sau1707@users.noreply.github.com>
2022-06-21 11:42:43 +0200

    meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,28 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from "next";
+import Head from "next/head";
 
-import NavBar from '../components/navabar/navbar'
-import Switch from "../components/index/switch"
-import Title from "../components/index/title"
-import Footbar from '../components/footbar/footbar'
-import About from '../components/index/about'
+import NavBar from "../components/navabar/navbar";
+import Switch from "../components/index/switch";
+import Title from "../components/index/title";
+import Footbar from "../components/footbar/footbar";
+import About from "../components/index/about";
 
 const Home: NextPage = () => {
-    return (
-        <>
-            <Head>
-                <title> Home</title>
-            </Head>
-            <NavBar />
-            <Title />
-            <About />
-            <Switch />
-            <Footbar />
-        </>
-    )
-}
+	return (
+		<>
+			<Head>
+				<title> Home</title>
+				<meta charSet='utf-8' />
+				<meta name='description' content='Official LongLink website' />
+				<meta name='robots' content='longlink, xlonglink' />
+			</Head>
+			<NavBar />
+			<Title />
+			<About />
+			<Switch />
+			<Footbar />
+		</>
+	);
+};
 
-export default Home
+export default Home;
