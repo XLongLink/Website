905eb6ae5ff25e48ba0cdcd69f3bb32a71c145e5
Sau1707 <leonardo@saurwein.ch>
2022-07-01 13:23:29 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Script from 'next/script'
 
 import NavBar from "../components/navabar/navbar";
 import Switch from "../components/index/switch";
@@ -8,22 +9,31 @@ import Footbar from "../components/footbar/footbar";
 import About from "../components/index/about";
 
 const Home: NextPage = () => {
-	return (
-		<>
-			<Head>
-				<title> Home</title>
-				<meta charSet='utf-8' />
-				<meta name='description' content='Official LongLink website' />
-				<meta name='author' content='LongLink' />
-				<meta name='keywords' content='longlink, xlonglink' />
-			</Head>
-			<NavBar />
-			<Title />
-			<About />
-			<Switch />
-			<Footbar />
-		</>
-	);
+    return (
+        <>
+            <Head>
+                <title> Home</title>
+                <meta charSet='utf-8' />
+                <meta name='description' content='Official LongLink website' />
+                <meta name='author' content='LongLink' />
+                <meta name='keywords' content='longlink, xlonglink' />
+            </Head>
+            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-W8T9TZZDTK"></Script>
+            <Script id="google-analytics" strategy="afterInteractive">
+                {
+                    `window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-W8T9TZZDTK');
+                `}
+            </Script>
+            <NavBar />
+            <Title />
+            <About />
+            <Switch />
+            <Footbar />
+        </>
+    );
 };
 
 export default Home;
